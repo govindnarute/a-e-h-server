fc5ead660f70f6c39b051418d35f6e39d5fa0aa5
GOVIND NARUTE -X (gnarute - ZENSAR TECHNOLOGIES INC at Cisco) <gnarute@cisco.com>
2022-12-25 22:00:56 +0530

    Changes for users

--- routes/Users.js
+++ routes/Users.js
@@ -4,10 +4,14 @@ const User=require("../model/User")
 
 router.get("/",async (req,res)=>{
 
-    
+
     try{
         const data = await User.find();
-        res.json(data)
+        res.json({
+            success:true,
+            message:"User fetch successfully",
+            data:data
+        })
     }
     catch(error){
         res.status(500).json({message: error.message})
@@ -22,14 +26,15 @@ router.post('/',async (req, res) => {
     })
 
     try {
-        const data = await User.find({username:req.body.username});
+        const user = await User.find({username:req.body.username});
        
-        if(data.length>0){
+        if(user.length>0){
             res.status(400).json({message: 'Record already present'})
             return
         }
         const dataToSave = data.save();
-        res.status(200).json(dataToSave)
+        res.status(200).json({message: 'Record saved successfully',success:true
+    })
     }
     catch (error) {
         res.status(400).json({message: error.message})
@@ -37,5 +42,51 @@ router.post('/',async (req, res) => {
 })
 
 
+router.post("/login",async(req,res)=>{
+  const username=req.body.username
+  const password=req.body.password
+try{
+const user= await User.find({username:username,password:password})
+if(user.length>=0){
+res.json({
+    success:true,
+    message:"User login successfully",
+    user:user[0]
+})
+}else{
+    res.json({
+        success:false,
+        message:"User not found"
+    }) 
+}
+}catch(error){
+    res.status(400).json({message: error.message})
+}
+
+})
+
+router.delete("/:userId",async(req,res)=>{
+let userId=req.params.userId
+try{
+    const user= await User.findByIdAndDelete({_id:userId})
+    console.log(user)
+    if(user){
+    res.json({
+        success:true,
+        message:"User deleted successfully",
+        user:user
+    })}else{
+        res.json({
+            success:false,
+            message:"User not found."
+        })
+    }
+
+
+}catch(error){
+    res.status(400).json({message: error.message})
+}
+
+})
 
 module.exports=router
